80009909a9f4a4e8a56a93efa4ea8205c0a3c5de
Sushant Sharma <sushant20.sharma00@gmail.com>
2024-09-03 09:33:05 +0530

    updated

--- components/TrendingVideoCard.tsx
+++ components/TrendingVideoCard.tsx
@@ -10,6 +10,7 @@ import {
 import { Models } from "react-native-appwrite";
 import * as Animatable from "react-native-animatable";
 import { useState } from "react";
+// import { Video, ResizeMode } from "expo-av";
 
 const playIcon = require("../assets/icons/play.png");
 
@@ -38,26 +39,35 @@ const TrendingVideoCard = ({
   post: Models.Document;
   activeItem: any;
 }) => {
+  const [play, setPlay] = useState(false);
+
   return (
     <Animatable.View
       className="mr-5 my-5"
       animation={activeItem === post.$id ? zoomIn : (zoomOut as any)}
       duration={500}
     >
-      <TouchableOpacity activeOpacity={0.6}>
-        <View className="relative flex justify-center items-center rounded-lg">
-          <Image
-            source={{ uri: post.thumbnail }}
-            className="w-52 h-72 rounded-lg opacity-60"
-            resizeMode="cover"
-          />
-          <Image
-            source={playIcon}
-            className="w-12 h-12 absolute"
-            resizeMode="contain"
-          />
-        </View>
-      </TouchableOpacity>
+      {play ? (
+        <Video
+          source={{ uri: post.videoUrl }}
+          resizeMode={ResizeMode.CONTAIN}
+        />
+      ) : (
+        <TouchableOpacity activeOpacity={0.6} onPress={() => setPlay(true)}>
+          <View className="relative flex justify-center items-center rounded-lg">
+            <Image
+              source={{ uri: post.thumbnail }}
+              className="w-52 h-72 rounded-lg opacity-60"
+              resizeMode="cover"
+            />
+            <Image
+              source={playIcon}
+              className="w-12 h-12 absolute"
+              resizeMode="contain"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
     </Animatable.View>
   );
 };
